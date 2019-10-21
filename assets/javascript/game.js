c13fe5c027f0dcc988612f7c29ae113dbbefe6bf
eason <eason15530@gmail.com>
2019-10-21 17:09:08 -0400

    all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     // Selects a random number, which should be between 19 - 120
-    var randomNum = Math.floor(Math.random() * 101 + 19)
+    var randomNum = Math.floor(Math.random() * 102) + 19
     
     // shows this random number to HTML corresponding position.
     $("#randomNumber").text(randomNum);
 
     // Sets up random numbers for each crystal, which should be between 1 - 12
-    var num1 = Math.floor(Math.random() * 11 + 1)
-    var num2 = Math.floor(Math.random() * 11 + 1)
-    var num3 = Math.floor(Math.random() * 11 + 1)
-    var num4 = Math.floor(Math.random() * 11 + 1)
+    var num1 = Math.floor(Math.random() * 12) + 1
+    var num2 = Math.floor(Math.random() * 12) + 1
+    var num3 = Math.floor(Math.random() * 12) + 1
+    var num4 = Math.floor(Math.random() * 12) + 1
 
     // shows the number of wins
     var wins = 0;
@@ -25,28 +25,28 @@ $(document).ready(function () {
     $("#scoreCounter").text(userScore);
 
 
-    //resets the game
+    //initializes the game
     function reset() {
-        randomNum = Math.floor(Math.random() * 101 + 19);
+        randomNum = Math.floor(Math.random() * 102) + 19;
         
         $('#randomNumber').text(randomNum);
 
-        num1 = Math.floor(Math.random() * 11 + 1);
-        num2 = Math.floor(Math.random() * 11 + 1);
-        num3 = Math.floor(Math.random() * 11 + 1);
-        num4 = Math.floor(Math.random() * 11 + 1);
+        num1 = Math.floor(Math.random() * 12) + 1;
+        num2 = Math.floor(Math.random() * 12) + 1;
+        num3 = Math.floor(Math.random() * 12) + 1;
+        num4 = Math.floor(Math.random() * 12) + 1;
         userScore = 0;
         $("#scoreCounter").text(userScore);
     }
     
-    //adds the wins to the userTotal
+    //adds the wins to the winCounter
     function winCount() {
         wins++;
         $("#winCounter").text(wins);
         reset();
     }
 
-    //addes the losses to the userTotal
+    //adds the losses to the lossCounter
     function lossCount() {
         losses++;
         $("#lossCounter").text(losses);
@@ -54,13 +54,13 @@ $(document).ready(function () {
     }
 
 
-    //sets up click for crystals
+    //sets up clicks for crystals
     $("#btn1").on("click", function () {
         userScore += num1;
         
         $("#scoreCounter").text(userScore);
         //sets win/lose conditions
-        if (userScore == randomNum) {
+        if (userScore === randomNum) {
             winCount();
         }
         else if (userScore > randomNum) {
@@ -71,7 +71,7 @@ $(document).ready(function () {
         userScore += num2;
         
         $("#scoreCounter").text(userScore);
-        if (userScore == randomNum) {
+        if (userScore === randomNum) {
             winCount();
         }
         else if (userScore > randomNum) {
@@ -83,7 +83,7 @@ $(document).ready(function () {
         
         $("#scoreCounter").text(userScore);
         //sets win/lose conditions
-        if (userScore == randomNum) {
+        if (userScore === randomNum) {
             winCount();
         }
         else if (userScore > randomNum) {
@@ -95,7 +95,7 @@ $(document).ready(function () {
         
         $("#scoreCounter").text(userScore);
 
-        if (userScore == randomNum) {
+        if (userScore === randomNum) {
             winCount();
         }
         else if (userScore > randomNum) {
